a17ba45c5334d89e57b1bae8c0f1057bff708a9a
GooseHonk14 <blanchard.beaux@grinnell-k12.org>
2021-11-26 20:03:31 +0000

    hey

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
@@ -9,14 +9,14 @@ def index():
 
 @app.route('/game', methods=['POST', 'GET'])
 def game():
-   return render_template('game.html')
+    if request.method == 'POST':
+        return redirect('https://5000-lavender-crocodile-z392xfsm.ws-us17.gitpod.io/game/update/{{id}}')
+    return render_template('game.html')
 
 
 @app.route('/game/update/<int:id>', methods=['POST', 'GET'])
 def update(id):
-    if request.method == 'POST':  
-       newthing = id
-    return newthing
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
