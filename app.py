0d0c0ed8dda4038ddb7de05894db18930814001c
GooseHonk14 <blanchard.beaux@grinnell-k12.org>
2021-11-30 18:57:27 +0000

    hi

--- app.py
+++ app.py
@@ -10,13 +10,16 @@ def index():
 @app.route('/game', methods=['POST', 'GET'])
 def game():
     if request.method == 'POST':
-        return redirect('https://5000-lavender-crocodile-z392xfsm.ws-us17.gitpod.io/game/update/{{id}}')
-    return render_template('game.html')
+        if request.form["button"] == 1:
+            return "It worked"
+        elif request.form["button"] == '1':
+            return "It worked as a string"
+        else:
+            return "it didn't work"
+    else:
+        return render_template('game.html')
 
 
-@app.route('/game/update/<int:id>', methods=['POST', 'GET'])
-def update(id):
-    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
