60ae89e0dbaf3dfb3ff028be98c05fc4b9ac633a
GooseHonk14 <blanchard.beaux@grinnell-k12.org>
2021-12-01 20:57:47 +0000

    WOOOOO

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
-
-
+x = "X"
+o = "O"
+blank = " "
+board = [[blank, blank, blank],
+        [blank, blank, blank],
+        [blank, blank, blank]]
 
 @app.route('/')
 def index():
@@ -10,14 +14,29 @@ def index():
 @app.route('/game', methods=['POST', 'GET'])
 def game():
     if request.method == 'POST':
-        if request.form["button"] == 1:
-            return "It worked"
-        elif request.form["button"] == '1':
-            return "It worked as a string"
+        user_input = int(request.form["button"])
+        if user_input == 1:
+            board[0][0] = x
+        elif user_input == 2:
+            board[0][1] = x
+        elif user_input == 3:
+            board[0][2] = x
+        elif user_input == 4:
+            board[1][0] = x
+        elif user_input == 5:
+            board[1][1] = x
+        elif user_input == 6:
+            board[1][2] = x
+        elif user_input == 7:
+            board[2][0] = x
+        elif user_input == 8:
+            board[2][1] = x
+        elif user_input == 9:
+            board[2][2] = x
         else:
-            return "it didn't work"
+            return "Something went wrong"
     else:
-        return render_template('game.html')
+        return render_template('game.html', board = board)
 
 
 
